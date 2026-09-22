089c5c81db315b45922dfa4a1f0f1c0f99489808
KMRN <muhammadkamrantaj@gmail.com>
2026-09-22 10:04:46 -0700

    Add villains data to villains.js

---
+++ spider-verse-archive/data/villains.js
@@ -0,0 +1,63 @@
+const villains = [
+
+  {
+    name: "Green Goblin",
+    ability: "Enhanced technology",
+    threat: "HIGH"
+  },
+
+  {
+    name: "Doctor Octopus",
+    ability: "Mechanical tentacles",
+    threat: "HIGH"
+  },
+
+  {
+    name: "Venom",
+    ability: "Symbiote physiology",
+    threat: "HIGH"
+  },
+
+  {
+    name: "Kingpin",
+    ability: "Power and resources",
+    threat: "HIGH"
+  },
+
+  {
+    name: "The Spot",
+    ability: "Dimensional portals",
+    threat: "CRITICAL"
+  },
+
+  {
+    name: "Vulture",
+    ability: "Flight technology",
+    threat: "MEDIUM"
+  },
+
+  {
+    name: "Electro",
+    ability: "Electric manipulation",
+    threat: "HIGH"
+  },
+
+  {
+    name: "Mysterio",
+    ability: "Illusion technology",
+    threat: "HIGH"
+  },
+
+  {
+    name: "The Prowler",
+    ability: "Stealth technology",
+    threat: "HIGH"
+  },
+
+  {
+    name: "The Tinkerer",
+    ability: "Advanced engineering",
+    threat: "MEDIUM"
+  }
+
+];
