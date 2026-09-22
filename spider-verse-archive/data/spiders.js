5b2e7b16dd7f927713d5f014cac571ddd0bdad5f
KMRN <muhammadkamrantaj@gmail.com>
2026-09-22 10:03:11 -0700

    Create data structure for Spider-Person characters
    Added a collection of Spider-Person characters with their traits and descriptions.

---
+++ spider-verse-archive/data/spiders.js
@@ -0,0 +1,110 @@
+const spiders = [
+
+  {
+    name: "Peter Parker",
+
+    alias: "Spider-Man",
+
+    earth: "Earth-616",
+
+    traits: [
+      "Responsibility",
+      "Science",
+      "Humor"
+    ],
+
+    description:
+      "The classic wall-crawler whose greatest strength is his determination to keep helping people."
+  },
+
+
+  {
+    name: "Miles Morales",
+
+    alias: "Spider-Man",
+
+    earth: "Earth-1610",
+
+    traits: [
+      "Venom Blast",
+      "Camouflage",
+      "Empathy"
+    ],
+
+    description:
+      "A Spider-Man with bio-electric abilities, camouflage and a talent for improvisation."
+  },
+
+
+  {
+    name: "Gwen Stacy",
+
+    alias: "Ghost-Spider",
+
+    earth: "Earth-65",
+
+    traits: [
+      "Agility",
+      "Music",
+      "Independence"
+    ],
+
+    description:
+      "A highly capable Spider-Person known for her agility and unique approach to the Spider mantle."
+  },
+
+
+  {
+    name: "Miguel O'Hara",
+
+    alias: "Spider-Man 2099",
+
+    earth: "Earth-928",
+
+    traits: [
+      "2099 Tech",
+      "Leadership",
+      "Enhanced Physiology"
+    ],
+
+    description:
+      "A futuristic Spider-Man operating in a technologically advanced world."
+  },
+
+
+  {
+    name: "Hobie Brown",
+
+    alias: "Spider-Punk",
+
+    earth: "Earth-138",
+
+    traits: [
+      "Rebellion",
+      "Guitar",
+      "Chaos"
+    ],
+
+    description:
+      "A punk-rock Spider-Person who prefers tearing down systems rather than following them."
+  },
+
+
+  {
+    name: "Pavitr Prabhakar",
+
+    alias: "Spider-Man India",
+
+    earth: "Earth-50101",
+
+    traits: [
+      "Agility",
+      "Luck",
+      "Style"
+    ],
+
+    description:
+      "A Mumbai-based Spider-Person with a distinct interpretation of the Spider mantle."
+  }
+
+];
